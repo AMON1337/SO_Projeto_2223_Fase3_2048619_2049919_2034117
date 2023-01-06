c3447bff686c3f4b99931f94b810e635e081476b
TomMendonca <tomendonca01@gmail.com>
2023-01-06 15:46:49 +0000

    Variaveis monitor

--- monitor_config.c
+++ monitor_config.c
@@ -1,10 +1,43 @@
 #include "unix.h" //Tem as bibliotecas comuns do projeto
 #define MAXLINE 512
 
-// Usamos os sockets dados nas aulas.
-
+// Usamos os sockets dados nas aulas
 /* Servidor do tipo socket stream.
    Manda linhas recebidas de volta para o cliente */
+
+//Variáveis monitor
+//Número atual de pessoas na zona(Z) ou na fila da zona(FZ)
+
+//Z0 - Discoteca
+int numZ0 = 0;
+int numFZ0 = 0;
+
+//Z1 - Pista pública
+int numZ1 = 0;
+int numFZ1 = 0;
+
+//Z2 - Zona VIP
+int numZ2 = 0;
+int numFZ2 = 0;
+
+//Z3 - WC
+int numZ3 = 0;
+int numFZ3 = 0;
+
+//Z4 - Restaurante
+int numZ4 = 0;
+int numFZ4 = 0;
+
+//Total de pessoas que já entraram na discoteca
+int totalDisco;
+//Total de pessoas que saíram da discoteca
+int saidaDisco;
+//Número de pessoas VIP dentro da discoteca
+int totalVIPS;
+
+//Variáveis simulador
+int Acontecimento;
+
 
 str_echo(sockfd) int sockfd;
 {
@@ -70,20 +103,20 @@ str_echo(sockfd) int sockfd;
         system("clear"); // Limpar Monitor anterior
         printf(" ________________________________________ \n");
         printf("(_______________INFORMAÇÃO_______________)\n");
-        printf("( Entrada: #x         Fila: #y           )\n");
+        printf("( Discoteca: %d       Fila: %d           )\n",numZ0,numFZ0);
         printf("(                                        )\n");
-        printf("( Pista Publica: #x   Fila: #y           )\n");
+        printf("( Pista Publica: %d   Fila: %d           )\n",numZ1,numFZ1);
         printf("(                                        )\n");
-        printf("( Zona VIP: #x        Fila: #y           )\n");
+        printf("( Zona VIP: %d        Fila: %d           )\n",numZ2,numFZ2);
         printf("(                                        )\n");
-        printf("( WC:#x               Fila: #y           )\n");
+        printf("( WC: %d              Fila: %d           )\n",numZ3,numFZ3);
         printf("(                                        )\n");
-        printf("( Restaurante: #x     Fila: #y           )\n");
+        printf("( Restaurante: %d     Fila: %d           )\n",numZ4,numFZ4);
         printf("(________________________________________)\n");
         printf("(___________Informação__Global___________)\n");
-        printf("( Entraram na Discoteca: #x              )\n");
-        printf("( Sairam da discoteca: #y                )\n");
-        printf("( Total de VIPs: #z                      )\n");
+        printf("( Entraram na Discoteca: %d              )\n",totalDisco);
+        printf("( Sairam da discoteca: %d                )\n",saidaDisco);
+        printf("( Total de VIPs: %d                      )\n",totalVIPS);
         printf("(________________________________________)\n");
 
         /* Lê uma linha do socket */ //<-- Funcionalidade Antiga
@@ -98,5 +131,99 @@ str_echo(sockfd) int sockfd;
            um caracter! */
         if (writen(sockfd, line, n) != n)
             err_dump("str_echo: writen error");
+
+        switch (Acontecimento)
+        {
+        // Abertura da Discoteca
+        case 60:
+            break;
+        // Encerramento da Discoteca
+        case 69:
+            return;
+            break;
+        // Entrada para a Discoteca
+        case 00:
+            numFZ0--;
+            numZ0++;
+            totalDisco++;
+            break;
+        // Entrada pista de dança
+        case 01:
+            numFZ1--;
+            numZ1++;
+            break;
+        // Entrada zona VIP
+        case 02:
+            numFZ2--;
+            numZ2++;
+            break;
+        // Entrada na WC
+        case 03:
+            numFZ3--;
+            numZ3++;    
+            break;
+        // Entrada no restaurante
+        case 04:
+            numFZ4--;
+            numZ4++;
+            break;
+        // Espera na fila - Discoteca
+        case 10:
+            numZ0++;
+            break;
+        // Espera na fila - Pista de dança
+        case 11:
+            numZ1++;
+            break;
+        // Espera na fila - Zona VIP
+        case 12:
+            numZ2++;
+            break;
+        // Espera na fila - WC
+        case 13:
+            numZ3++;
+            break;
+        // Desistência da fila - Discoteca
+        case 20:
+            numFZ0--;
+            break;
+        // Desistência da fila - Pista de Dança
+        case 21:
+            numFZ1--;
+            break;
+        // Desistência da fila - Zona VIP
+        case 22:
+            numFZ2--;
+            break;
+        // Desistência da fila - WC
+        case 23:
+            numFZ3--;
+            break;
+        // Saída - Discoteca
+        case 30:
+            numZ0--;
+            saidaDisco++;
+            break;
+        // Saída - Pista de Dança
+        case 31:
+            numZ1--;
+            break;
+        // Saída - Zona VIP
+        case 32:
+            numZ2--;
+            break;
+        // Saída- WC
+        case 33:
+            numZ3--;
+            break;
+        // Saída - restaurante
+        case 34:
+            numZ4--;
+            break;
+        default:
+            printf("ERRO: Acontecimento inválido.\n");
+            break;
+        }
     }
 }
+
